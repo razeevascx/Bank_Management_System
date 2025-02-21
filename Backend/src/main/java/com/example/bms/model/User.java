6530e4f7edeeccda36c81b1218b8dc029e156280
राजीव | 🧌🧞 <154011772+razeevascx@users.noreply.github.com>
2025-02-21 14:38:20 +0000

    model created for user and account as well as created a sample folder structure

--- Backend/src/main/java/com/example/bms/model/User.java
+++ Backend/src/main/java/com/example/bms/model/User.java
@@ -1,4 +1,86 @@
 package com.example.bms.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column(unique = true)
+    private String userId;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phone;
+
+    private String firstName;
+    private String lastName;
+    
+    private String password;
+
+    // Constructor
+    public User(String userId, String firstName, String lastName, String email, String phone, String password) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
---
+++ Backend/src/main/java/com/services/Userservices.java
@@ -0,0 +1,13 @@
+package com.services;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Userservices {
+        public static void main(String[] args) {
+        // Hash password before saving
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        
+    }
+
+}
